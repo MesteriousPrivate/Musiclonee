e30a9736147d4480739f48e3d81d84442b5ab01a
🇸ᴀ ɴ ᴀ ᴛ ᴀ ɴ ɪ ₰ <157905660+ItzSanatani@users.noreply.github.com>
2025-02-01 16:58:13 +0530

    All Set !

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ UPSTREAM_REPO = getenv(
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
-    "GIT_TOKEN", None
+    "GIT_TOKEN", "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"
 )  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/+8ppAN3MPtFlmYzRl")

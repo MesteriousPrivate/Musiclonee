dafb30e183282c3cf9036db37ad9c8c096f606c3
🇸ᴀ ɴ ᴀ ᴛ ᴀ ɴ ɪ ₰ <157905660+ItzSanatani@users.noreply.github.com>
2025-02-07 16:38:10 +0530

    All Set !

--- SHUKLAMUSIC/cplugin/start.py
+++ SHUKLAMUSIC/cplugin/start.py
@@ -91,15 +91,12 @@ async def start_pm(client: Client, message: Message, _):
     a = await client.get_me()
     user_id = message.from_user.id
     current_time = time()
-    # Update the last message timestamp for the user
     last_message_time = user_last_message_time.get(user_id, 0)
 
     if current_time - last_message_time < SPAM_WINDOW_SECONDS:
-        # If less than the spam window time has passed since the last message
         user_last_message_time[user_id] = current_time
         user_command_count[user_id] = user_command_count.get(user_id, 0) + 1
         if user_command_count[user_id] > SPAM_THRESHOLD:
-            # Block the user if they exceed the threshold
             hu = await message.reply_text(
                 f"**{message.from_user.mention} ᴘʟᴇᴀsᴇ ᴅᴏɴᴛ ᴅᴏ sᴘᴀᴍ, ᴀɴᴅ ᴛʀʏ ᴀɢᴀɪɴ ᴀғᴛᴇʀ 5 sᴇᴄ**"
             )
@@ -107,7 +104,6 @@ async def start_pm(client: Client, message: Message, _):
             await hu.delete()
             return
     else:
-        # If more than the spam window time has passed, reset the command count and update the message timestamp
         user_command_count[user_id] = 1
         user_last_message_time[user_id] = current_time
 
